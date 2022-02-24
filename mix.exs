96bbbdc74c8538a199bb90f09a9ed5165921d131
RaymondLoranger <Raymond.Loranger@bell.net>
2022-02-23 21:55:35 -0500

    0.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gallows.MixProject do
   def project do
     [
       app: :gallows,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
